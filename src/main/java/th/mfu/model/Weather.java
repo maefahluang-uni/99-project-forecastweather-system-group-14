8dbfe354d366132e422cde7e23f702191b7b7859
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2023-10-31 21:26:51 +0700

    Added Weather Model

--- src/main/java/th/mfu/model/Weather.java
+++ src/main/java/th/mfu/model/Weather.java
@@ -1,5 +1,101 @@
 package th.mfu.model;
 
 public class Weather {
-    
+    private String city;
+    private String country;
+    private String countryISO;
+    private String timeZone;
+    private String description;
+    private String tempFelt;
+    private Double tempMin;
+    private Double tempMax;
+    private Double pressure;
+    private Double humidity;
+
+    public Weather() {
+        // empty constructor for JSON Parsing
+    }
+
+    // Getter & Setter
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryISO() {
+        return countryISO;
+    }
+
+    public void setCountryISO(String countryISO) {
+        this.countryISO = countryISO;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTempFelt() {
+        return tempFelt;
+    }
+
+    public void setTempFelt(String tempFelt) {
+        this.tempFelt = tempFelt;
+    }
+
+    public Double getTempMin() {
+        return tempMin;
+    }
+
+    public void setTempMin(Double tempMin) {
+        this.tempMin = tempMin;
+    }
+
+    public Double getTempMax() {
+        return tempMax;
+    }
+
+    public void setTempMax(Double tempMax) {
+        this.tempMax = tempMax;
+    }
+
+    public Double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(Double pressure) {
+        this.pressure = pressure;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+
 }
