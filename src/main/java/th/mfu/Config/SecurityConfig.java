7a9d32bfe4bed62e0c07fa0e468e7532e4285878
Kritsasoft <6531503005@lamduan.mfu.ac.th>
2023-10-31 22:34:00 +0700

    SecurityConfig

--- src/main/java/th/mfu/Config/SecurityConfig.java
+++ src/main/java/th/mfu/Config/SecurityConfig.java
@@ -1,8 +1,24 @@
 package com.bouali.oauth2.social.security;
 
-import org.springframewok.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.EnableConSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
      
+     @Bean
+     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+          http
+               .csrf()
+               .disable()
+               .authorizeHttpRequests()
+               .anyRequest()
+               .authenticated()
+               .and()
+               .oauth2Login()
+               ;
+               return http.build();
+     }
 }
