e3d69aeba490262381a3e2f2a0baf7b0f6725be0
2547phumiphat <6531503117@lamduan.mfu.ac.th>
2023-11-02 22:03:41 +0700

    update Usercontroller Baseexception Userservice

--- src/main/java/th/mfu/controller/UserController.java
+++ src/main/java/th/mfu/controller/UserController.java
@@ -1,17 +1,38 @@
 package th.mfu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import th.mfu.model.User;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+
+     @Autowired
+     private final User user;
+     public UserController(User user) {
+          this.user = user;
+     }
+
      @GetMapping
      public ResponseEntity<String> hello() {
           return new ResponseEntity<String>("", HttpStatus.OK);
+     }
+     /*@GetMapping
+     @RequestMapping(value = "/Login")
+     public ResponseEntity<String> login() {
+           return new ResponseEntity<String>("", HttpStatus.OK);}/* */
+     @PostMapping
+     @RequestMapping(value ="/Sighup")
+     public String Sighup(@RequestBody User request) {
+          return "Received" + request;
      }
 
 }
---
+++ src/main/java/th/mfu/service/UserService.java
@@ -0,0 +1,11 @@
+/*package th.mfu.service;
+import org.springframework.stereotype.Service;
+import th.mfu.model.User;
+
+@Service
+public class UserService {
+    public String Sighup(User request) {
+        return "";
+    }
+    
+}/* */
