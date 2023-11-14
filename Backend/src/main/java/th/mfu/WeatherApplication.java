75328c621d291f1f7d7bf5c22921f0fa63a49444
Kritsasoft <6531503005@lamduan.mfu.ac.th>
2023-11-14 22:35:45 +0700

    delnoti

--- Backend/src/main/java/th/mfu/WeatherApplication.java
+++ Backend/src/main/java/th/mfu/WeatherApplication.java
@@ -1,9 +1,5 @@
 package th.mfu;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,14 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherApplication extends SpringBootServletInitializer {
-    @Autowired
-    private EmailSenderService EmailSenderService;
     public static void main(String[] args) {
         SpringApplication.run(WeatherApplication.class, args);
     }
     
-    @EventListener(ApplicationReadyEvent.class)
-    public void triggerMail() {
-        senderService.sendEmail("6531503117@lamduan.mfu.ac.th", "สวัสดีจากกิ้กแฟ้ง", "เบนจี้กั้มมม");
-    }
 }
